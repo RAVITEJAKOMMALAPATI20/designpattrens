a891e824bb82aa69efe4715aff4f2a35d220024d
Raviteja <raviteja.kommalapati@sjsu.edu>
2019-01-07 00:44:55 -0800

    First Commit

---
+++ designpattrenscode/src/main/java/com/moviecentral/demo/DesignpattrenscodeApplication.java
@@ -0,0 +1,14 @@
+package com.moviecentral.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpattrenscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignpattrenscodeApplication.class, args);
+	}
+
+}
+
